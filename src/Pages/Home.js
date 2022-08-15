1505689432d0c943badb48c644a76727858dfcc3
Birhane <Birhanegebrial1@gmail.com>
2022-08-16 02:47:55 +0300

    start working

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import Header from "../inc/Header";
+import "../styles/Style.css";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <h1> home page </h1>
+    </div>
+  );
+}
+export default Home;

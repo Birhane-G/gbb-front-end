ad6e7a161cad87058184b85528ca0b3f56b0e913
Birhane <Birhanegebrial1@gmail.com>
2022-08-21 21:11:11 +0300

    Birhane

--- src/Pages/Service.js
+++ src/Pages/Service.js
@@ -1,8 +1,27 @@
-
 function Service() {
   return (
-    <div>Service</div>
-  )
+    <section className="service">
+      <div className="title">
+        <h2>Service</h2>
+      </div>
+      <div className="service-cont">
+        <div className="box-cont">
+          <div className="box">
+            <p>Web Development</p>
+          </div>
+          <div className="box">
+            <p>Android Development</p>
+          </div>
+          <div className="box">
+            <p>System Development</p>
+            </div>
+          <div className="box">
+            <p>Logo Design</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Service;

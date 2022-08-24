7b061750225241f1397e4994e1f5dd7fd5df3d9d
Gemechisgk <gemechis.gk@gmail.com>
2022-08-24 15:05:30 +0300

    test push

--- src/Pages/Center.js
+++ src/Pages/Center.js
@@ -7,6 +7,12 @@ function Center() {
         <div className="text">
           <h3>Hello</h3>
           <p>
+            Lorem Ipsum adalah abcontoh teks atau dummy dalam industri percetakan
+            dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi
+            standar contoh teks sejak tahun 1500an, saat seorang tukang cetak
+            yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya
+            untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan
+            selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik
           </p>
         </div>
         <div className="get">
